d35e53315096d4ff60cd409d8d9870b891718c47
amine's macbook <macbook@MacBook-Pro-4.local>
2024-11-26 18:20:18 +0100

    bazgho

--- get_next_line.c
+++ get_next_line.c
@@ -1,25 +1,81 @@
 #include "get_next_line.h"
 
-char	*read_file(int fd, char *str)
+char	*read_line(int fd, char *str)
 {
 	char	*buffer;
 	ssize_t	read_bytes;
-	char	*tmp;
 
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if(!buffer)
 		return NULL;
+	if(str == NULL)
+	{
+		str = malloc(1);
+		if(!str)
+			return (/*free(buffer),*/, NULL);
+		str[0] = '\0';
+	}
 	read_bytes = 1;
-	while(read_bytes != 0 && !strchr(str, '\n'))
+	while(read_bytes != 0 && !ft_strchr(str , '\n'))
 	{
 		read_bytes = read(fd, buffer, BUFFER_SIZE);
-		if(read_bytes <= 0)
+		if(read_bytes == 0)
 			break;
+		if(read_bytes == -1)
+			return(free(str), free(buffer), NULL);
 		buffer[read_bytes] = '\0';
+		str = ft_strjoin(str, buffer);
 	}
+	return(free(buffer), str);
+}
+char *extract_line(char *str)
+{
+	char *new_line_position;
+	char *valid_line;
+	size_t sub_line_lenght;
+	char *tmp;
+
+	new_line_position = ft_strchr(str, '\n');
+	valid_line = NULL;
+	if(!str)
+		return(NULL);
+	if(new_line_position)
+	{
+		sub_line_lenght = (size_t)(new_line_position - str + 1);
+		valid_line = ft_substr(str, 0, sub_line_lenght);
+		tmp = ft_strdup(new_line_position + 1);
+		free(str);
+		str = tmp;
+	}
+	else
+	{
+			valid_line = ft_substr(str, 0, ft_strlen(str));
+			free(str);
+			// str = NULL;
+	}
+	return(valid_line);
 }
 
 char *get_next_line(int fd)
 {
-	static char
+	static char *tr;
+	char *line;
+
+	if(fd < 0 || BUFFER_SIZE <= 0)
+		return NULL;
+	tr = read_line(fd, tr);
+	if(tr == NULL)
+		return NULL;
+	line = extract_line(tr);
+	return(line);
 }
+// int main()
+// {
+// 	int fd = open("amine.txt", O_RDONLY);
+
+// 	char *a;
+// 	while((a = get_next_line(fd))!= NULL)
+// 	{
+// 		printf("%s", a);
+// 	}
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -10,7 +10,12 @@
 # include <string.h>
 # include <unistd.h>
 
-size_t ft_strlen(const char *s);
-char *ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*read_line(int fd, char *str);
+char	*extract_line(char *str);
+char	*ft_strdup(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
-
+#endif
